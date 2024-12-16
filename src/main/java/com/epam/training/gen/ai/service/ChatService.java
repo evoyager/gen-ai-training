2f0906bab263fc2191c318468dd72078e8918e36
Ievgen Gusar <evgeniygusar@gmail.com>
2024-12-16 13:40:35 +0200

    Revert "Module4 task4"

--- src/main/java/com/epam/training/gen/ai/service/ChatService.java
+++ src/main/java/com/epam/training/gen/ai/service/ChatService.java
@@ -1,10 +1,9 @@
 package com.epam.training.gen.ai.service;
 
 import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.implementation.CollectionUtil;
 import com.microsoft.semantickernel.orchestration.InvocationContext;
-import com.microsoft.semantickernel.orchestration.PromptExecutionSettings;
-import com.microsoft.semantickernel.semanticfunctions.KernelFunction;
-import com.microsoft.semantickernel.semanticfunctions.KernelFunctionArguments;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
 import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,56 +20,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChatService {
 
+    private final ChatCompletionService chat;
     private final Kernel kernel;
-    private final ChatHistory chatHistory;
+    private final ChatHistory history;
     private final InvocationContext invocationContext;
-    private final PromptExecutionSettings promptExecutionSettings;
 
-    public ChatService(Kernel kernel, InvocationContext invocationContext, PromptExecutionSettings promptExecutionSettings) {
+    public ChatService(ChatCompletionService chat, Kernel kernel, InvocationContext invocationContext) {
+        this.chat = chat;
         this.kernel = kernel;
-        this.chatHistory = new ChatHistory();
+        this.history = new ChatHistory();
         this.invocationContext = invocationContext;
-        this.promptExecutionSettings = promptExecutionSettings;
     }
 
-    public String getChatResponse(String prompt) {
-        var response = kernel.invokeAsync(getChat())
-                .withArguments(getKernelFunctionArguments(prompt, chatHistory))
-                .withPromptExecutionSettings(promptExecutionSettings)
-                .withInvocationContext(invocationContext)
+    public String getChatResponse(String input) {
+        history.addUserMessage(input);
+
+        var result = chat.getChatMessageContentsAsync(history, kernel, invocationContext)
                 .block();
-        chatHistory.addUserMessage(prompt);
-        chatHistory.addAssistantMessage(response.getResult());
-        log.info("AI answer: " + response.getResult());
-        return response.getResult();
-    }
 
-    /**
-     * Creates a kernel function for generating a chat response using a predefined prompt template.
-     * <p>
-     * The template includes the chat history and the user's message as variables.
-     *
-     * @return a {@link KernelFunction} for handling chat-based AI interactions
-     */
-    private KernelFunction<String> getChat() {
-        return KernelFunction.<String>createFromPrompt("""
-                        {{$chatHistory}}
-                        <message role="user">{{$request}}</message>""")
-                .build();
-    }
+        String response = CollectionUtil.getLastOrNull(result).getContent();
+        history.addAssistantMessage(response);
+        log.info("Response: {}", response);
 
-    /**
-     * Creates the kernel function arguments with the user prompt and chat history.
-     *
-     * @param prompt the user's input
-     * @param chatHistory the current chat history
-     * @return a {@link KernelFunctionArguments} instance containing the variables for the AI model
-     */
-    private KernelFunctionArguments getKernelFunctionArguments(String prompt, ChatHistory chatHistory) {
-        return KernelFunctionArguments.builder()
-                .withVariable("request", prompt)
-                .withVariable("chatHistory", chatHistory)
-                .build();
+        return response;
     }
 
 }
