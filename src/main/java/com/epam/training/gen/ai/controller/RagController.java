626563a20b6ab23aa2907b41b4c93b02d39d09bd
Ievgen Gusar <ievgen_gusar@epam.com>
2024-12-20 13:40:46 +0200

    Add endpoint to load documents

--- src/main/java/com/epam/training/gen/ai/controller/RagController.java
+++ src/main/java/com/epam/training/gen/ai/controller/RagController.java
@@ -1,7 +1,9 @@
 package com.epam.training.gen.ai.controller;
 
 import com.epam.training.gen.ai.dto.RagChatRequest;
+import com.epam.training.gen.ai.dto.RagDocumentsRequest;
 import com.epam.training.gen.ai.service.RagService;
+import dev.langchain4j.store.embedding.IngestionResult;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +18,16 @@ public class RagController {
 
     private final RagService ragService;
 
-    //TODO: create endpoint to load knowledge from request sources
+    @PostMapping("/rag/documents")
+    public ResponseEntity<String> loadDocuments(@RequestBody RagDocumentsRequest request) {
+        String path = request.getPath();
+        IngestionResult response = ragService.loadDocuments(path);
+        return ResponseEntity.ok(String.format("Documents successfully loaded to RAG from path: [\"%s\"].\n %s", path, response.tokenUsage()));
+    }
 
     @PostMapping("/rag/chat")
     public ResponseEntity<String> chatViaRag(@RequestBody RagChatRequest request) {
         String response = ragService.chatViaRag(request.getInput());
-        return ResponseEntity.ok(String.format("Response from RAG: {%s}", response));
+        return ResponseEntity.ok(String.format("Response from AI using RAG: {%s}", response));
     }
 }
--- src/main/java/com/epam/training/gen/ai/service/RagService.java
+++ src/main/java/com/epam/training/gen/ai/service/RagService.java
@@ -10,6 +10,7 @@ import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.IngestionResult;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,14 +32,20 @@ public class RagService {
 
     @Value("${client-openai-endpoint}") String endpoint;
 
-    public String chatViaRag(String prompt) {
-        List<Document> documents = FileSystemDocumentLoader.loadDocuments(
-                "./src/main/resources/documents"
-        );
-        //TODO: use real embedding store like Qdrant instead of in-memory
-        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+    InMemoryEmbeddingStore<TextSegment> embeddingStore;
 
+    public RagService() {
+        this.embeddingStore = new InMemoryEmbeddingStore<>();
+    }
+
+    public IngestionResult loadDocuments(String path) {
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(path);
+        //TODO: use real embedding store like Qdrant instead of in-memory
+//        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        return EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+    }
+
+    public String chatViaRag(String prompt) {
         ChatLanguageModel chatModel = OpenAiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName(GPT_4_O_MINI)
